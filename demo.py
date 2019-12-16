b3deb98d06897fadfcc2d25e347c61f9320e5568
dearfad <dearfad@sina.com>
2019-12-16 15:39:11 +0800

    update

---
+++ demo.py
@@ -0,0 +1,31 @@
+from flask import Flask, request
+import joblib
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def predict():
+
+    age = request.form.get('age')
+    sex = request.form.get('sex')
+    height = request.form.get('height')
+    weight = request.form.get('weight')
+    
+    predict = model.predict([[age, sex, height, weight]])
+
+    form = '<form method="POST" action="http://127.0.0.1:5000" name="testform"> \
+            Age: <input type="text" name="age" value="50"> \
+            Sex: <input type="text" name="sex" value="1"> \
+            Height: <input type="text" name="height" value="150"> \
+            Weight: <input type="text" name="weight" value="60"> \
+            <input type="submit" value="START"></form>'
+    
+    result = f'<H1>result: {predict[0]}</H1>' 
+
+    return form + result
+
+if __name__ == '__main__':
+
+    model = joblib.load("demo.pkl")
+
+    app.run(debug=True)
